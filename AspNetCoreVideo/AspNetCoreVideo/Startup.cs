d880bcb4173fb2e36ba222c159407e0e285b1871
2LearnNewStuff <44630300+2LearnNewStuff@users.noreply.github.com>
2018-10-31 14:21:46 +0800

    Till Page  34

--- AspNetCoreVideo/AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/AspNetCoreVideo/Startup.cs
@@ -1,33 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreVideo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCoreVideo
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            
+            // services.AddSingleton<IMessageService, HardcodedMessageService>();
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //// throw new Exceptiton("Fake Exception");
+                await context.Response.WriteAsync(msg.GetMessage());
             });
         }
     }
